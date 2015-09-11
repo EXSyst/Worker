aee91fa0fa91a0554100ead83b1f2b4d684d84a4
Nicolas L. / Exter-N <exter-n@exter-n.fr>
2015-09-11 17:13:35 +0200

    Fixed a bug in the SocketFactory when there is a custom error handler

--- Internal/SocketFactory.php
+++ Internal/SocketFactory.php
@@ -12,11 +12,13 @@ final class SocketFactory
 
     public static function createServerSocket($socketAddress, $socketContext = null)
     {
+        set_error_handler(null);
         if ($socketContext !== null) {
             $socket = @stream_socket_server($socketAddress, $errno, $errstr, STREAM_SERVER_BIND | STREAM_SERVER_LISTEN, $socketContext);
         } else {
             $socket = @stream_socket_server($socketAddress, $errno, $errstr, STREAM_SERVER_BIND | STREAM_SERVER_LISTEN);
         }
+        restore_error_handler();
         if ($socket === false) {
             throw new Exception\BindOrListenException($errstr, $errno);
         }
@@ -29,11 +31,13 @@ final class SocketFactory
         if ($timeout === null) {
             $timeout = intval(ini_get('default_socket_timeout'));
         }
+        set_error_handler(null);
         if ($socketContext !== null) {
             $socket = @stream_socket_client($socketAddress, $errno, $errstr, $timeout, STREAM_CLIENT_CONNECT, $socketContext);
         } else {
             $socket = @stream_socket_client($socketAddress, $errno, $errstr, $timeout, STREAM_CLIENT_CONNECT);
         }
+        restore_error_handler();
         if ($socket === false) {
             throw new Exception\ConnectException($errstr, $errno);
         }
